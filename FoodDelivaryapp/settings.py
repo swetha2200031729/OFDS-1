693764e8f4c44e19d2d0caea75b0832ce2d7af8b
swetha <swevara@gmail.com>
2024-04-27 01:23:08 +0530

    db changes

--- FoodDelivaryapp/settings.py
+++ FoodDelivaryapp/settings.py
@@ -75,18 +75,12 @@ WSGI_APPLICATION = 'FoodDelivaryapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.postgresql',
- 'NAME': 'Food_Delivery',
- 'USER': 'postgres',
- 'PASSWORD': 'admin',
- 'HOST': 'localhost',
- 'PORT': '5432',
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
